fa40fb458b65e8784186abc8b40a5fb43a6ccab9
Gaurang <gaurangp69@gmail.com>
2025-05-17 17:27:37 +0530

    changed base url

--- src/constants/mainContent.js
+++ src/constants/mainContent.js
@@ -6,16 +6,16 @@ export const MainContent = {
   logo: logo,
 };
 export const backendConfig = {
-  base: "http://192.168.1.60:5047/api",
-  origin: "http://192.168.1.60:5047",
+  // base: "http://192.168.1.60:5047/api",
+  // origin: "http://192.168.1.60:5047",
   
   // base: "https://cnfp6kct-5049.inc1.devtunnels.ms/api",
   // origin: "https://cnfp6kct-5049.inc1.devtunnels.ms/",
   // base: "https://adm.api.smartchainstudio.in/api",
   // origin: "https://adm.api.smartchainstudio.in",
 
-  // base: "https://asimon.api.smartchainstudio.in/api",
-  // origin: "https://asimon.api.smartchainstudio.in",
+  base: "https://asimon.api.smartchainstudio.in/api",
+  origin: "https://asimon.api.smartchainstudio.in",
 };
 
 const token = localStorage.getItem("token");
